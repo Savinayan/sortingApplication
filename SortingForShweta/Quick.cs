4e01b76fbe59325902996198a03f656ce8620910
savinayan sangam <savinayan@gmail.com>
2020-01-07 21:43:33 +0530

    Commit Quick and Merge

--- SortingForShweta/Quick.cs
+++ SortingForShweta/Quick.cs
@@ -10,7 +10,45 @@ namespace SortingAlgorithms
         {
             int[] outarr = new int[arr.Length];
             // put your code here
-            outarr = arr;// delete this line when you code
+            public static int Partition(int[] numbers, int left, int right)
+            {
+                int pivot = numbers[left];
+                while (true)
+                {
+                    while (numbers[left] < pivot)
+                        left++;
+                    while (numbers[right] > pivot)
+                        right--;
+                    if (left < right)
+                    {
+                        int temp = numbers[right];
+                        numbers[right] = numbers[left];
+                        numbers[left] = temp;
+                    }
+                    else
+                    {
+                        return right;
+                    }
+                }
+            }
+        }
+             public static void SortQuick(int[] arr, int left, int right)
+            {
+                // For Recusrion  
+                if (left < right)
+                {
+                    int pivot = Partition(arr, left, right);
+                    if (pivot > 1)
+                        SortQuick(arr, left, pivot - 1);
+                    if (pivot + 1 < right)
+                        SortQuick(arr, pivot + 1, right);
+                }
+            }
+        public static void Main(string[] args)
+        {
+            SortQuick(numbers, 0, max - 1);
+        }
+           outarr = arr;// delete this line when you code
             return outarr;
         }
     }
